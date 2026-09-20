69c7475bcf68ffe3ade87e6f1aac741b940d9015
kenic___ <cinekgamingcod@gmail.com>
2026-09-20 19:10:28 +0200

    Loader

---
+++ src/app/projects/[id]/loading.tsx
@@ -0,0 +1,19 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { AppShell } from "@/components/layout/app-shell";
+
+export default function ProjectLoading() {
+  return (
+    <div className="p-6 space-y-6">
+      <div className="flex items-center justify-between">
+        <Skeleton className="h-10 w-64" />
+        <Skeleton className="h-10 w-32" />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <Skeleton className="h-32 w-full" />
+        <Skeleton className="h-32 w-full" />
+        <Skeleton className="h-32 w-full" />
+      </div>
+      <Skeleton className="h-[500px] w-full" />
+    </div>
+  );
+}
